5c9e4ad3e202ff8a27b5b7f9f823a80bebd51926
Noah Simai <noah@simai.de>
2023-11-19 15:35:17 +0100

    added config  to  gitignore

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from voice_automatisation import Assistant
 
 assistant = Assistant(
-    "Söder",
+    "Assistent",
     [],
     model_path=r"voice_automatisation/models/vosk-model-small-de-0.15/vosk-model-small-de-0.15",
     verbose=True,
